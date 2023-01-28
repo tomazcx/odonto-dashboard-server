d76acc3dec21386bbbc9c8eeb30c6a2be6cc04c0
tomazcx <tomazcx06@gmail.com>
2023-01-28 14:49:37 -0300

    Added production port

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,3 +1,3 @@
 import {app} from "./app"
 
-app.listen(3000, () => console.log('Servidor iniciado na porta 3000'))
+app.listen(process.env.PORT ?? 3000, () => console.log(`Servidor iniciado na porta ${process.env.PORT ?? 3000}`))
